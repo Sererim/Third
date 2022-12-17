62b109e4e6752441b70c7064ac05dec9888762a5
Sererim <danielleymanw@gmail.com>
2022-12-17 18:55:24 +0300

    Initial commit of the second exercies. Skeleton of a class and a main function.

---
+++ second.py
@@ -0,0 +1,21 @@
+
+class NumberFloat():
+    
+    def __init__(self) -> None:
+        pass
+    
+    def search_min_max() -> None:
+        pass
+    
+    def min_float() -> None:
+        pass
+    
+    def max_float() -> None:
+        pass
+    
+    def out_min_max() -> float:
+        pass
+    
+
+def main() -> int:
+    
