41e9df57b1c657815b94594cf90c4c4f660ac6f9
Sererim <danielleymanw@gmail.com>
2022-12-17 17:55:45 +0300

    First exercies is done.

--- first.py
+++ first.py
@@ -10,17 +10,46 @@ class ListMulp:
         self.entered = entered
         self.out = out
         
+    # Get input for the entered list 
     def getinput(self) -> None:
         
         foo: int = 0
         bar: int = 0
-        foo = input(f"{Utils.message[1]}")
-        print(f"{Utils.message[2]}")
+        foo = int(input(f"{Utils.message(1)}"))
+        print(f"{Utils.message(2)}")
         for i in range(0,foo):
             bar = int(input())
             self.entered.append(bar)
     
-    def getout(self) -> list[int]:
+    # Make out list
+    def makeoutlist(self):
         
-            
+        j: int = len(self.entered) - 1
+        i: int = 0
         
+        while True:
+            self.out.append(self.entered[j] * self.entered[i])
+            j -= 1
+            i += 1
+            if i > j:
+                break
+    
+    def takeoutlist(self) -> list[int]:
+        return self.out
+    
+
+def main() -> int:
+    
+    mulp: object = ListMulp()
+    print(f"{Utils.message(0)}")
+    while True:
+        mulp.getinput()
+        mulp.makeoutlist()
+        print(mulp.takeoutlist())
+        if Utils.terminate():
+            break
+    return 0
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -5,11 +5,22 @@ class Utils:
     def message(num: int) -> str:
         
         words: list[str] =[
-            "Program is running",
-            "Enter the size of a list.",
-            "Enter numbers.",
-            "Enter a number.",
-            "Do you want to treminate the program Y/y ?."
+            "Program is running.\n",
+            "Enter the size of a list.\n",
+            "Enter numbers.\n",
+            "Enter a number.\n",
+            "Do you want to treminate the program Y/y ?.\n"
         ]
         
-        return words[num]
+        return words[num] 
+    
+    @staticmethod
+    def terminate() -> bool:
+        
+        control: str = "NULL"
+        while True:
+            control = input(Utils.message(4))
+            if control == "Y" or control == 'y':
+                return True
+
+    
